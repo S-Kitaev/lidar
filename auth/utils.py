f626bd03a0d2e6e0b8ec5456f72626e10b31c04c
skitaev <skitaev@example.com>
2025-04-18 22:59:39 +0300

    fead: add new entities for auth, first test

--- auth/utils.py
+++ auth/utils.py
@@ -1,3 +1,4 @@
+import bcrypt
 import jwt
 
 from lidar.core.config import settings
@@ -26,3 +27,20 @@ def decode_jwt(
         public_key,
         algorithms=[algorithm])
     return decoded
+
+def hash_password(
+    password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
+
+def validate_password(
+    password: str,
+    hashed_password: bytes,
+) -> bool:
+    return bcrypt.checkpw(
+        password=password.encode(),
+        hashed_password=hashed_password,
+    )
---
+++ main.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+import uvicorn
+
+from core.config import settings
+from api_v1 import router as router_v1
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
+#app.include_router(items_router)
+app.include_router(users_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
