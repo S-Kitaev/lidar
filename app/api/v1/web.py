61d7e8a94a9c3ee254572a8ae9d70205c8418e40
skitaev <skitaev@example.com>
2025-04-20 23:32:07 +0300

    style: some fixes

--- app/api/v1/web.py
+++ app/api/v1/web.py
@@ -147,7 +147,7 @@ async def home(
         return RedirectResponse(url="/login", status_code=status.HTTP_303_SEE_OTHER)
 
     return templates.TemplateResponse(
-        "main.html",
+        "home_main.html",
         {"request": request, "username": user.user_name}
     )
 
